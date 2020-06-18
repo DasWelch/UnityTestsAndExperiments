3ff719b624e2e80ab28587e7469f326af50a8944
DasWelch <35710104+DasWelch@users.noreply.github.com>
2020-06-18 15:27:20 -0400

    testing using git with Unity

---
+++ TestAndExperiment/Assets/MovePlannet.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Unity.Mathematics;
+using UnityEngine;
+
+public class MovePlannet : MonoBehaviour
+{
+    public Rigidbody2D rb;
+    public float orbitDistance;
+    public float rpm;
+    public float rotationRateDeg;
+
+    private float angRad;
+    
+    
+
+    void Start()
+    {
+        angRad = 0;
+        SetPosition();
+    }
+
+    void FixedUpdate()
+    {
+        // rpm - 6f is the degrees to travel in a minute
+        // Time.deltaTime adjusts for updates and movement persecond
+        float degreeChange = (rpm * 6f * Time.deltaTime);// rpm in degreee accounting for deltatime 
+        float radChange = degreeChange * (math.PI / 180);
+
+
+        angRad += radChange;
+
+        SetPosition();
+    }
+
+    void SetPosition() {
+
+        float xpos = orbitDistance * math.cos(angRad);
+        float ypos = orbitDistance * math.sin(angRad);
+
+        rb.rotation += rotationRateDeg * Time.deltaTime;
+        rb.position = new Vector2(xpos,ypos);
+    }
+}
