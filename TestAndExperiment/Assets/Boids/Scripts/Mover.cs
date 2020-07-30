5d914876e1413487d49e0b9d77c11b9e23a9b09b
DasWelch <35710104+DasWelch@users.noreply.github.com>
2020-07-30 11:12:34 -0400

    fixed some stuff on windows

--- TestAndExperiment/Assets/Boids/Scripts/Mover.cs
+++ TestAndExperiment/Assets/Boids/Scripts/Mover.cs
@@ -11,7 +11,7 @@ public class Mover : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        transform.position = new Vector2(0, 0);
+        //transform.position = new Vector2(0, 0);
     }
 
     // Update is called once per frame
