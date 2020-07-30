065bad248cf7d2ac634222eaa1aaef9bfc1cbf96
DasWelch <welchie123@live.com>
2020-07-30 09:57:36 -0400

    adding in important stuff to use on pc

--- TestAndExperiment/Assets/Boids/Scripts/Mover.cs
+++ TestAndExperiment/Assets/Boids/Scripts/Mover.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class Mover : MonoBehaviour
 {
+
+    public Vector3 acc;
+    public Vector3 vel;
+    public Vector3 pos;
     // Start is called before the first frame update
     void Start()
     {
@@ -12,9 +16,25 @@ public class Mover : MonoBehaviour
 
     // Update is called once per frame
     void Update()
-    {
-        
+    { 
+    
     }
 
+    void FixedUpdate() 
+    {
+        var move = Forward2d();
+        gameObject.transform.position += move * Time.deltaTime;//gameObject.transform.forward * Time.deltaTime;
+    }
+
+    Vector3 Forward2d() 
+    {
+        Vector3 forward = new Vector3(0, 0, 0);
+
+        float ang = gameObject.transform.position.z;
+        forward.x = Mathf.Cos(ang);
+        forward.y = Mathf.Sin(ang);
+
+        return forward;
+    }
 
 }
